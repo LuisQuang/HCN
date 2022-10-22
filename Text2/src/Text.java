021b10da99eeb84b29b9f6ef70a92cd0e4245bee
LuisQuang <115853900+LuisQuang@users.noreply.github.com>
2022-10-22 08:48:03 +0700

    VD1

---
+++ Text2/src/Text.java
@@ -0,0 +1,44 @@
+public class Text {
+	private String text;
+
+	public Text(String text) {
+		this.text = text;
+	}
+
+	public void upperCaseChar() {
+		System.out.println("Chuyen in hoa chuoi nhap vao: " + text.toUpperCase());
+	}
+
+	public void reverse() {
+		System.out.print("Chuoi dao nguoc: ");
+		for (int i = text.length() - 1; i >= 0; i--) {
+			System.out.print(text.charAt(i));
+		}
+	}
+
+	public void upperCaseFirstChars() {
+		String kq = "";
+		for (int i = 0; i < text.length(); i++) {
+			String nam = text.charAt(i) + "";
+			if (i == 0 || text.charAt(i - 1) == ' ') {
+				nam = nam.toUpperCase();
+			}
+			kq += nam;
+		}
+		System.out.println("\nViet hoa cac chu cai dau: " + kq);
+	}
+
+	public void reverseCase() {
+		String kq = "";
+		for (int i = 0; i < text.length(); i++) {
+			String nam = text.charAt(i) + "";
+			String namMoi = nam.toUpperCase();
+			if (namMoi.equals(nam)) {
+				namMoi = nam.toLowerCase();
+			}
+			kq += namMoi;
+		}
+
+		System.out.println("Dao nguoc hoa-thuong: " + kq);
+	}
+}
